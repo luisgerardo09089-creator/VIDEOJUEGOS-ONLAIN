37f105454e3024879638f9a4a0334b2dfecca6b5
luisgerardo09089-creator <luisgerardo09.089@gmail.com>
2026-04-16 19:25:48 -0600

    Create app.js

---
+++ js /app.js
@@ -0,0 +1,11 @@
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        const destino = document.querySelector(this.getAttribute("href"));
+
+        destino.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
